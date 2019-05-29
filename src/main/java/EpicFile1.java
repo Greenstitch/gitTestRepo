b4cba91264b6118e512df2db86d503f91b0ee761
Jan <JanFreirich@gmail.com>
2019-05-29 17:43:49 +0200

    added sout epic 2 here

--- src/main/java/EpicFile1.java
+++ src/main/java/EpicFile1.java
@@ -1,6 +1,6 @@
 public class EpicFile1 {
     public static void main(String[] args) {
-        System.out.println("epic2 here");
-        System.out.println("another push from 2");
+        System.out.println("epic1 here");
+        System.out.println("another push from 1");
     }
 }
